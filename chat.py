4c56d7bf9056e90fa08fe2f52dfceaea98e06394
sri-sowkhya <167790553+sri-sowkhya@users.noreply.github.com>
2025-03-23 21:04:37 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -19,7 +19,7 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-if prompt := st.chat_input("Say something..."):
+if prompt := st.chat_input("Ask to Summarize it! Keep your response brief."):
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
